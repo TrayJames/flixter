de1f630df897c2e8e8ae98039a3a3d983a4673e0
Tra-Vaughn James <tvj@icebook.fios-router.home>
2021-02-24 21:47:51 -0500

    latest code

---
+++ Lab1/Lab1/Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1App.swift
+//  Lab1
+//
+//  Created by Tra-Vaughn James on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrueFlix/TrueFlix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  TrueFlix
+//
+//  Created by Tra-Vaughn James on 2/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
